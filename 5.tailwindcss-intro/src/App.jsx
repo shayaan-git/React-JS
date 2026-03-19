41444b7c403f5525fff74bf7e5ebaea59c8bb71e
shayaan-git <mshayaan24@gmail.com>
2026-03-19 21:47:24 +0530

    tailwind-CSS Intro

---
+++ 5.tailwindcss-intro/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-blue-300 bg-amber-700 h-20">App</h1>
+    </div>
+  );
+};
+
+export default App;
